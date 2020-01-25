ee7de09a8b7e47c8f360b7e6693304f86a31438e
Derek <derek.ojeda-centeno@polymtl.ca>
2020-01-25 17:49:27 -0500

    Start refactoring of Point2D (Cleanup still needed)

--- tp1/src/tp1/Point2d.java
+++ tp1/src/tp1/Point2d.java
@@ -10,7 +10,9 @@ public class Point2d extends AbstractPoint {
     }
 
     // Cree un point a partir d'un vecteur de donnees
-    public Point2d(Double[] vector) { super(vector.clone()); }
+    public Point2d(Double[] vector) {
+        super(new Double[]{vector[0],vector[1]});
+    }
 
     public Double X() { return vector[X];}
     public Double Y() { return vector[Y];}
@@ -19,43 +21,48 @@ public class Point2d extends AbstractPoint {
     @Override
     public Point2d translate(Double[] translateVector) {
 
-        Point2d point = new Point2d(this.vector);
+//        Point2d point = new Point2d(vector);
+//
+//        if (point.vector.length == translateVector.length){
+//            for (int i = 0; i < point.vector.length ; i++) {
+//                point.vector[i] += translateVector[i];
+//            }
+//        }
+//        return point;
 
-        if (point.vector.length == translateVector.length){
-            for (int i = 0; i < point.vector.length ; i++) {
-                point.vector[i] += translateVector[i];
-            }
-        }
-        return point;
+        return new Point2d(PointOperator.translate(vector.clone(),translateVector));
+
     }
 
-    // TODO prendre un point et appliquer la translation. // FAIT?
+    // Prendre un point et appliquer la translation.
     public Point2d translate(Point2d translateVector) {
-        if (this.vector.length == translateVector.vector.length){
-            for (int i = 0; i < this.vector.length ; i++) {
-                this.vector[i] += translateVector.vector[i];
-            }
-        }
-        return this;
+//        if (this.vector.length == translateVector.vector.length){
+//            for (int i = 0; i < this.vector.length ; i++) {
+//                this.vector[i] += translateVector.vector[i];
+//            }
+//        }
+        return translate(translateVector.vector.clone());
     }
 
     // Prend un vecteur de donnees et appliquer la rotation.
     @Override
     public Point2d rotate(Double[][] rotationMatrix) {
-        Double[] rotatedVector = new Double[vector.length];
-        double coordinate;
-        // Assuming square matrix
-        if (vector.length == rotationMatrix.length) {
-            for (int i = 0; i < rotationMatrix.length; i++) {
-                coordinate = 0;
-                for (int j = 0; j < rotationMatrix[i].length; j++) {
-                    coordinate += rotationMatrix[i][j] * vector[j];
-                }
-                rotatedVector[i] = coordinate;
-            }
-            return new Point2d(rotatedVector);
-        }
-        return this;
+//        Double[] rotatedVector = new Double[vector.length];
+//        double coordinate;
+//        // Assuming square matrix
+//        if (vector.length == rotationMatrix.length) {
+//            for (int i = 0; i < rotationMatrix.length; i++) {
+//                coordinate = 0;
+//                for (int j = 0; j < rotationMatrix[i].length; j++) {
+//                    coordinate += rotationMatrix[i][j] * vector[j];
+//                }
+//                rotatedVector[i] = coordinate;
+//            }
+//            return new Point2d(rotatedVector);
+//        }
+//        return this;
+
+        return new Point2d(PointOperator.rotate(vector.clone(),rotationMatrix));
     }
 
     // TODO prendre un angle de rotation, creer une matrice et appliquer la rotation. FAIT?
@@ -71,41 +78,43 @@ public class Point2d extends AbstractPoint {
     // Prendre un facteur de division et l'appliquer
     @Override
     public Point2d divide(Double divider) {
-        Point2d point = new Point2d(this.vector);
-        if (divider != 0){
-            for (int i = 0; i < point.vector.length; i++) {
-                point.vector[i] /= divider;
-            }
-        }
-        return point;
+//        Point2d point = new Point2d(this.vector);
+//        if (divider != 0){
+//            for (int i = 0; i < point.vector.length; i++) {
+//                point.vector[i] /= divider;
+//            }
+//        }
+//        return point;
+        return new Point2d(PointOperator.divide(vector.clone(),divider));
     }
 
-    // Prendre un facteur de multiplication et l'appliquer
+    // Prend un facteur de multiplication et l'applique
     @Override
     public Point2d multiply(Double multiplier) {
         Point2d point = new Point2d(this.vector);
-
-        for (int i = 0; i < point.vector.length ; i++) {
-            point.vector[i] *= multiplier;
-        }
-
-        return point;
+//
+//        for (int i = 0; i < point.vector.length ; i++) {
+//            point.vector[i] *= multiplier;
+//        }
+//
+//        return point;
+        return new Point2d(PointOperator.multiply(vector.clone(),multiplier));
     }
 
-    // TODO prendre un facteur d'addition et l'appliquer. FAIT?
+    // Prendre un facteur d'addition et l'applique
     @Override
     public Point2d add(Double adder) {
-        Point2d point = new Point2d(this.vector);
-        for (int i = 0; i < point.vector.length; i++) {
-            point.vector[i] += adder;
-        }
-        return point;
+//        Point2d point = new Point2d(this.vector);
+//        for (int i = 0; i < point.vector.length; i++) {
+//            point.vector[i] += adder;
+//        }
+//        return point;
+        return new Point2d(PointOperator.add(vector.clone(),adder));
     }
 
     // Creer un nouveau point
     @Override
     public Point2d clone() {
-        Point2d objetClone = new Point2d(this.vector);
-        return objetClone;
+        return new Point2d(vector.clone());
     }
 }
