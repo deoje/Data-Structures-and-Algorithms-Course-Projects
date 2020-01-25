7b3d9ed50faa7d6cec3e18354b1eb1f7ba348e51
Derek <derek.ojeda-centeno@polymtl.ca>
2020-01-25 17:50:53 -0500

    Cleaned up comments

--- tp1/src/tp1/Point2d.java
+++ tp1/src/tp1/Point2d.java
@@ -20,48 +20,17 @@ public class Point2d extends AbstractPoint {
     // Prend un vecteur de donnees et applique la translation.
     @Override
     public Point2d translate(Double[] translateVector) {
-
-//        Point2d point = new Point2d(vector);
-//
-//        if (point.vector.length == translateVector.length){
-//            for (int i = 0; i < point.vector.length ; i++) {
-//                point.vector[i] += translateVector[i];
-//            }
-//        }
-//        return point;
-
         return new Point2d(PointOperator.translate(vector.clone(),translateVector));
-
     }
 
     // Prendre un point et appliquer la translation.
     public Point2d translate(Point2d translateVector) {
-//        if (this.vector.length == translateVector.vector.length){
-//            for (int i = 0; i < this.vector.length ; i++) {
-//                this.vector[i] += translateVector.vector[i];
-//            }
-//        }
         return translate(translateVector.vector.clone());
     }
 
     // Prend un vecteur de donnees et appliquer la rotation.
     @Override
     public Point2d rotate(Double[][] rotationMatrix) {
-//        Double[] rotatedVector = new Double[vector.length];
-//        double coordinate;
-//        // Assuming square matrix
-//        if (vector.length == rotationMatrix.length) {
-//            for (int i = 0; i < rotationMatrix.length; i++) {
-//                coordinate = 0;
-//                for (int j = 0; j < rotationMatrix[i].length; j++) {
-//                    coordinate += rotationMatrix[i][j] * vector[j];
-//                }
-//                rotatedVector[i] = coordinate;
-//            }
-//            return new Point2d(rotatedVector);
-//        }
-//        return this;
-
         return new Point2d(PointOperator.rotate(vector.clone(),rotationMatrix));
     }
 
@@ -78,13 +47,6 @@ public class Point2d extends AbstractPoint {
     // Prendre un facteur de division et l'appliquer
     @Override
     public Point2d divide(Double divider) {
-//        Point2d point = new Point2d(this.vector);
-//        if (divider != 0){
-//            for (int i = 0; i < point.vector.length; i++) {
-//                point.vector[i] /= divider;
-//            }
-//        }
-//        return point;
         return new Point2d(PointOperator.divide(vector.clone(),divider));
     }
 
@@ -92,23 +54,12 @@ public class Point2d extends AbstractPoint {
     @Override
     public Point2d multiply(Double multiplier) {
         Point2d point = new Point2d(this.vector);
-//
-//        for (int i = 0; i < point.vector.length ; i++) {
-//            point.vector[i] *= multiplier;
-//        }
-//
-//        return point;
         return new Point2d(PointOperator.multiply(vector.clone(),multiplier));
     }
 
     // Prendre un facteur d'addition et l'applique
     @Override
     public Point2d add(Double adder) {
-//        Point2d point = new Point2d(this.vector);
-//        for (int i = 0; i < point.vector.length; i++) {
-//            point.vector[i] += adder;
-//        }
-//        return point;
         return new Point2d(PointOperator.add(vector.clone(),adder));
     }
 
