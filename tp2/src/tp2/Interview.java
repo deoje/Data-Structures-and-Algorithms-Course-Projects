917c6c21f8f2182bfd86b4cdfe77df69661f4406
Derek <derek.ojeda-centeno@polymtl.ca>
2020-02-17 13:07:24 -0500

    Fixed ConcurrentModificationError by creating a hack... a point is still missing for No Permutation

--- tp2/src/tp2/Interview.java
+++ tp2/src/tp2/Interview.java
@@ -1,5 +1,6 @@
 package tp2;
 
+import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.ArrayList;
@@ -12,35 +13,53 @@ public class Interview {
      * @return A collection containing all valid pairs with no permutations, but all combinations (empty collection if none found)
      */
     public Collection<MatchingPair> matchingPairs(Collection<Integer> values, Integer targetSum){
-        ArrayList<MatchingPair> collectionFinale = new ArrayList<MatchingPair>();
+        ArrayList<MatchingPair> collectionFinale = new ArrayList<>();
 
-
-        if (values.size() == 0 || targetSum == null){ //s'il manque une valeur alors on retourne une collection vide
+        //s'il manque une valeur alors on retourne une collection vide
+        if (values.size() == 0 || targetSum == null){
             return collectionFinale;
         }
 
-        for (Iterator<Integer> iterator = values.iterator(); iterator.hasNext();) { //On parcour le tableau pour additioner chacune de ses valeurs
-            int valeur_a = iterator.next(); //la premiere valeur à additioner
-            for (Iterator<Integer> iterator2 = values.iterator(); iterator2.hasNext(); ) { //On va faire l'addition avec chacune des valeurs
-                int valeur_b = iterator2.next(); //la deuxieme valeur a additioner
-                if ((valeur_b + valeur_a == targetSum && valeur_b != valeur_a)) { //Si l'addtion marche
-                    // Doit trouver moyen pour si _2 et 2_ sont dans valeurs et target = 4 alors prendre la paire (_2, 2_)?
-                    MatchingPair pair = new MatchingPair(valeur_b, valeur_a); //creer la paire
-                    collectionFinale.add(pair);//ajouter la paire a la collection
+        //On parcours le tableau pour additioner chacune de ses valeurs
+        for (Iterator<Integer> iterator = values.iterator(); iterator.hasNext();) {
+            //la premiere valeur à additioner
+            int valeur_a = iterator.next();
+
+            //On va faire l'addition avec chacune des valeurs
+            for (Iterator<Integer> iterator2 = values.iterator(); iterator2.hasNext(); ) {
+                //la deuxieme valeur a additioner
+                int valeur_b = iterator2.next();
+
+                // FIXME: Trouver moyen pour si _2 et 2_ sont dans valeurs et target = 4 alors prendre la paire (_2, 2_)?
+                //Si l'addtion marche
+                if ((valeur_b + valeur_a == targetSum && valeur_b != valeur_a)) {
+                    //ajouter la paire a la collection
+                    collectionFinale.add(new MatchingPair(valeur_b, valeur_a));
                 }
+
             }
+
         }
+//        //Go over each pair
+//        for (MatchingPair pair_a : collectionFinale) {
+//            // FIRST pair of the possible couple ((a,b),(b,a))
+//            // SECOND pair of the possible couple ((a,b),(b,a))
+//            // si cas (a,b) et (b,a) on doit que guarder une
+//            // ERREUR : le remove adapte bien iterator 3 et 4 et la collection mais ca créé une erreur pour iterator3.next()... Why?
+//            collectionFinale.removeIf(pair_b -> pair_a.first == pair_b.second && pair_a.second == pair_b.first);
+//        }
+        int i = 0;
+        while (i != collectionFinale.size() / 2 - 1){
+            if (collectionFinale.size() == 0)
+                break;
 
-        for (Iterator<MatchingPair> iterator3 = collectionFinale.iterator(); iterator3.hasNext(); ) { //On compare toute la collection
-            MatchingPair pair_a = iterator3.next(); //la paire a comparer voir si y a (a,b) et (b,a)
-            for (Iterator<MatchingPair> iterator4 = collectionFinale.iterator(); iterator4.hasNext(); ) { //On compare a la paire
-                MatchingPair pair_b = iterator4.next(); //l'autre paire a comparer
-                if (pair_b.equals(pair_a) && pair_b != pair_a) { // si cas (a,b) et (b,a) on doit que guarder une
-                    iterator4.remove(); // ERREUR : le remove adapte bien iterator 3 et 4 et la collection mais ca créé une erreur pour iterator3.next()... Why?
-                }
-            }
-    }
+            MatchingPair pair_a = collectionFinale.get(i);
 
+            if (collectionFinale.removeIf(pair_b -> pair_a.first.equals(pair_b.second) && pair_a.second.equals(pair_b.first)))
+                i = 0;
+            
+            i++;
+        }
         return collectionFinale;
     }
 }
