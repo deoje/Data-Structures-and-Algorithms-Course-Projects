16eeb90a92b27145220157ff71b570d6e0c40ff5
unknown <alexandre.ramt@gmail.com>
2020-04-09 22:09:40 -0400

    dossier tp5

---
+++ tp5/src/Edge.java
@@ -0,0 +1,40 @@
+
+public class Edge {
+
+	private Node source;
+	private Node destination;
+	private int distance;
+	
+	public Edge () {}
+
+
+        public Edge(Node s, Node d) {
+		this.source = s;
+		this.destination = d;		
+	}
+
+	public Edge(Node s, Node d, int dist) {
+		this.source = s;
+		this.destination = d;
+                this.distance = dist ; 		
+	}
+     
+      	
+	public Node getSource() {
+		return source;
+	}
+	public void setSource(Node source) {
+		this.source = source;
+	}
+	public Node getDestination() {
+		return destination;
+	}
+	public void setDestination(Node destination) {
+		this.destination = destination;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	
+	
+}
